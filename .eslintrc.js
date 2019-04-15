6fc269189e3673406a0003ba7740e2b13b595dc4
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2019-04-15 10:26:31 +0200

    Fix eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,22 @@ module.exports = {
 	env: {
 		browser: true,
 		es6: true,
+		amd: true,
+		node: true,
+	},
+	globals: {
+		require: true,
 	},
 	extends: "eslint:recommended",
+	parser: "babel-eslint",
 	parserOptions: {
-		ecmaVersion: 2016,
+		ecmaVersion: 6,
+		sourceType: "module",
+		ecmaFeatures: {
+			jsx: true,
+			modules: true,
+			experimentalObjectRestSpread: true,
+		},
 	},
 	rules: {
 		indent: ["error", "tab"],
