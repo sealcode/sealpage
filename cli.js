c8465b602574d72b74b9cf400487d8f79124d389
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2019-04-15 12:19:00 +0200

    Add simple switch-case statement

--- cli.js
+++ cli.js
@@ -7,6 +7,26 @@ const init = () => {
 };
 
 const run = async () => {
+	switch (process.argv[2]) {
+	case undefined:
+		console.log(chalk.red('Please type build or watch'));
+		break;
+	case 'build':
+		console.log(
+			chalk.black.bgWhiteBright.bold(' Build %s '),
+			process.argv[3]
+		);
+		break;
+	case 'watch':
+		console.log(
+			chalk.black.bgYellowBright.bold(' Watch %s '),
+			process.argv[3]
+		);
+		break;
+	default:
+		break;
+	}
+
 	init();
 };
 
