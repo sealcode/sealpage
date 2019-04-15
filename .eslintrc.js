89ec212e13dc5846b7b5b3c74238b8828848f781
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2019-04-15 10:21:38 +0200

    Fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 5
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+	env: {
+		browser: true,
+		es6: true,
+	},
+	extends: "eslint:recommended",
+	parserOptions: {
+		ecmaVersion: 2016,
+	},
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+	},
 };
