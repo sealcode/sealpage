4ab976d14c67f7d6483d04372710e6a16e99e0d5
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2019-05-24 09:18:37 +0200

    Add server side rendering
    Summary: Ref T1508 Ref T1509
    
    Test Plan: Uruchomic, dodac kilka komponentow i sprawdzic ze server render dziala z preview
    
    Reviewers: #reviewers, kuba-orlik
    
    Reviewed By: #reviewers, kuba-orlik
    
    Maniphest Tasks: T1508, T1509
    
    Differential Revision: https://hub.sealcode.org/D621

--- admin-panel/body-page-editor/preview.jsx
+++ admin-panel/body-page-editor/preview.jsx
@@ -1,22 +1,22 @@
 const React = require('react');
 const { useState, useEffect } = React;
-const components_map = require('../../components');
 
-async function renderPreview(elements) {
-	let html = '';
-
-	for (const [componentName, componentProps] of elements) {
-		html += await components_map[componentName].render(componentProps);
-	}
-
-	return html;
+async function generateRenderedHTML(elements) {
+	return await fetch('/api/v1/render', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ body: elements }),
+	}).then(response => response.text());
 }
-module.exports = function Preview(props) {
+
+module.exports = function Preview({ elements }) {
 	const [html, setHtml] = useState('');
 
 	useEffect(() => {
-		renderPreview(props.elements).then(setHtml);
-	});
+		generateRenderedHTML(elements).then(data => setHtml(data));
+	}, [elements]);
 
 	return React.createElement('div', {
 		style: {
